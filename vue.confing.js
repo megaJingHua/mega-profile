7d18ff70c8d1d6161704924ad49dde842edd0acf
mega.chen <mega.chen@sshop.com.tw>
2020-12-21 15:17:09 +0800

    dist

--- vue.confing.js
+++ vue.confing.js
@@ -1,8 +1,17 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-        ? '/eric-project/'
-        : '/',
-
-        
- baseUrl: './vue'
-}
+        ? './'
+        : './',
+    outputDir: "dist",
+    lintOnSave: true,
+    runtimeCompiler: false,
+    productionSourceMap: false,
+    devServer: {
+        open: process.platform === "darwin",
+        port: 8000,
+        https: false,
+        hotOnly: false,
+        proxy: null,
+        disableHostCheck: true,
+    }
+};
