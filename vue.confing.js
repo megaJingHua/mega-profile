854f29da41f93e0ad9b3c731598c539a2d2e0c80
mega.chen <mega.chen@sshop.com.tw>
2020-12-21 15:27:41 +0800

    test

--- vue.confing.js
+++ vue.confing.js
@@ -1,17 +1,5 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-        ? './'
-        : './',
-    outputDir: "dist",
-    lintOnSave: true,
-    runtimeCompiler: false,
-    productionSourceMap: false,
-    devServer: {
-        open: process.platform === "darwin",
-        port: 8000,
-        https: false,
-        hotOnly: false,
-        proxy: null,
-        disableHostCheck: true,
-    }
-};
+        ? './vue'
+        : './vue',
+}
